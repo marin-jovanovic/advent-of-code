ee3b88b66c9bc3b806b3994cd127f08fb0c91349
Marin Jovanović <mj51760@fer.hr>
2020-12-04 10:31:50 +0100

    day 1

---
+++ 1/main.py
@@ -0,0 +1,12 @@
+file = open("1/input.txt", "r")
+lines = list();
+
+[lines.append(int(i[:-1])) for i in file.readlines()]
+
+[
+    [
+        [print(lines[i] * lines[j]) if lines[i] + lines[j] == 2020 else False]
+            for j in range(i, len(lines))
+    ]
+        for i in range(len(lines))
+]
