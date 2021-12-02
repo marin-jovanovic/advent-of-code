39e7e1f3da781fb4424d78ff1d9626954c5ca5c9
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-12-02 14:22:51 +0100

    :construction: wip

--- 2020/14/part_two.py
+++ 2020/14/part_two.py
@@ -1,53 +1,96 @@
-def dtb(data):
-    return bin(data).replace("0b", "")
+def decimalToBinary(n):
+    return bin(n).replace("0b", "")
+
+def transform(data):
+    pass
 
 
-if __name__ == '__main__':
-    raw_input = [line[:-1] for line in open("input2.txt").readlines()]
 
-    [print(i) for i in raw_input]
-    print()
 
-    while True:
-        try:
-            line = raw_input[0]
-        except IndexError:
-            break
+file = open("input.txt", "r")
+raw_input = list()
 
-        raw_input = raw_input[1:]
-        # print(line)
+[raw_input.append(i[:-1]) for i in file.readlines()]
 
-        mask = (line.split(" = "))[1]
-        print("mask:", mask)
+prev_mask = None
 
-        mem = []
+from collections import defaultdict
+main_d = {}
 
-        try:
-            while not raw_input[0].startswith("mask = "):
-                # print(raw_input[0])
-                # t = raw_input[0]
-                # t = (raw_input[0])[4:]
-                # print(t)
-                t = (((raw_input[0])[4:]).split("] = "))[0]
-                # print(t)
+for elem in raw_input:
+    # print(elem)
 
-                mem.append(dtb(int(t)))
+    if elem.startswith("mem"):
+        t = elem.split(" ")
+        new_v = int(t[2])
+        f = int(t[0][4:-1])
+        t = decimalToBinary(f).zfill(36)
+        print(t)
+        print(prev_mask)
+        print("+ =")
 
-                raw_input = raw_input[1:]
+        r_l= []
+        x_d = []
 
-        except IndexError:
-            pass
+        c =0
+        for f, s in zip(prev_mask, t):
+            # print(f, s)
+            f = str(f)
+            s = str(s)
+            if f == "0" and s == "0":
+                r_l.append(f)
+            elif f == "1" and s == "0":
+                r_l.append("1")
+            elif f == "X" and s == "0":
+                x_d.append(c)
+                r_l.append("X")
+            elif f == "0" and s == "1":
+                r_l.append("1")
+            elif f == "1" and s == "1":
+                r_l.append("1")
+            elif f == "X" and s == "1":
+                r_l.append("X")
+                x_d.append(c)
+            # print("".join(r_l))
+            c += 1
 
-        [print(i) for i in mem]
-        # print()
+        tmp  = "".join(r_l)
+        n_l = []
+        print(tmp)
+        t_2 = 2 **len(x_d)
+        print(t_2)
 
-        mask_rev = mask[::-1]
-        mem_rev = [i[::-1] for i in mem]
-
-        print("mask_rev: ", mask_rev)
-
-        print("mem_rev:", str(mem_rev))
-        # [print(i) for i in mem_rev]
         print()
+        for i in range(t_2):
+            n_l.append(tmp)
 
-        print(dtb(100))
+        t = []
+        for i in range(t_2):
+            b = bin(i)[2:]
+            t.append(b)
+        max_l = len(t[-1])
+
+        n_t= []
+        for i in t:
+            if not len(i) == max_l:
+                i = i.zfill(max_l)
+
+            n_t.append(i)
+
+        n = []
+        for f, s in zip (n_t, n_l):
+            for i in f:
+                s = s.replace("X", i, 1)
+            n.append(s)
+
+        # [print(i) for i in n]
+        for i in n:
+            t = int(i,2)
+            print(t)
+            main_d[t] = new_v
+        print(main_d)
+    else:
+        t = elem.split(" ")
+        prev_mask = t[2]
+
+print(sum(main_d.values()))
